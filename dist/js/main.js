4b62c3693ba216476b3bcec5d8a3f4926db94c0e
MONSOON <mousumi.cse05@gmail.com>
2020-05-23 22:28:05 +0600

    submit buform

--- dist/js/main.js
+++ dist/js/main.js
@@ -17,7 +17,7 @@ function toggleMenu() {
     menu.classList.add("show");
     menuNav.classList.add("show");
     menuBranding.classList.add("show");
-    navItems.forEach(item => item.classList.add("show"));
+    navItems.forEach((item) => item.classList.add("show"));
 
     //Set Menu State
     showMenu = true;
@@ -26,9 +26,36 @@ function toggleMenu() {
     menu.classList.remove("show");
     menuNav.classList.remove("show");
     menuBranding.classList.remove("show");
-    navItems.forEach(item => item.classList.remove("show"));
+    navItems.forEach((item) => item.classList.remove("show"));
 
     //Set Menu State
     showMenu = false;
   }
 }
+
+$("#contactForm").submit(function (e) {
+  var name = document.getElementById("inputName");
+  email = document.getElementById("inputEmail");
+  phone = document.getElementById("inputPhone");
+  subject = document.getElementById("inputSubject");
+  message = document.getElementById("inputMessage");
+  if (
+    !name.value ||
+    !email.value ||
+    !message.value ||
+    !phone.value ||
+    !subject.value
+  ) {
+    alertify.error("Please check your entries");
+  } else {
+    $.ajax({
+      method: "POST",
+      url: "https://formspree.io/optimistic.leo2020@gmail.com",
+      data: $("#contactForm").serialize(),
+      datatype: "json",
+    });
+    e.preventDefault();
+    $(this).get(0).reset();
+    alertify.success("Message sent");
+  }
+});
