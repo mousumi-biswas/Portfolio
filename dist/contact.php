3018f78e6073abaded05a667b7cfe43cbe66f67a
MONSOON <mousumi.cse05@gmail.com>
2020-05-23 19:43:58 +0600

    contact form added

---
+++ dist/contact.php
@@ -0,0 +1,25 @@
+<?php
+
+$name=$_REQUEST['name'];
+$email=$_REQUEST['email'];
+$phone=$_REQUEST['phone-number'];
+$subject=$_REQUEST['subject'];
+$message=$_REQUEST['message'];
+
+if(empty($name) || empty($email) || empty($phone) || empty($subject)|| empty($message))
+{
+        echo "Please fill all the fields";
+}
+else {
+        mail("optimistic.leo2020@gmail.com", subject, message);
+        echo "<script type='text/javascript'> alert('Your message sent successfully')
+        window.history.log(-1);
+        </script>";
+}
+
+
+
+
+
+
+?>
